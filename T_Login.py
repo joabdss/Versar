3e0383a1a779f41d89d4d4230f8ce3146e524302
S3n1nh4 <anajsena2002@gmail.com>
2025-07-25 16:21:08 -0300

    2. Efetuar o Login
    Parte 2:
    
    S preenche os dados e efetua o login
    
    Resultado:
    
    Redirecionamento para a próxima página
    
    Próxima Etapa:
    
    3. Clicar "Criar Módulo"

--- T_Login.py
+++ T_Login.py
@@ -7,12 +7,21 @@ from time import sleep
 caminho_driver = r"C:\Users\Sena\OneDrive\Desktop\webdriver\chromedriver-win32\chromedriver.exe"
 service = Service(caminho_driver)
 
+#Abrir Navegador e Site
 driver = webdriver.Chrome(service=service)
 driver.get("https://playful-torrone-162c28.netlify.app")
 sleep(2)
 
+#Clicar em LOGIN
 login_button = driver.find_element(By.XPATH, '//span[text()="Login"]')
 login_button.click()
+
+# Preenche os campos de login
+driver.find_element(By.NAME, "email").send_keys("qualityassurance@bertoni.com.br")
+driver.find_element(By.NAME, "password").send_keys("versar123")
+
+#Efetua o Login e entra na página
+driver.find_element(By.TAG_NAME, "form").submit()
 
 
 sleep(10)
