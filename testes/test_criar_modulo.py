152a09f39516b38f896b30f07e2a2664880fd28c
joabdss <joabe_silva_sena@hotmail.com>
2025-07-28 18:12:53 -0300

    revisao

--- testes/test_criar_modulo.py
+++ testes/test_criar_modulo.py
@@ -1,25 +1,29 @@
-def test_criar_modulo():
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.chrome.service import Service
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    from webdriver_manager.chrome import ChromeDriverManager
-    from time import sleep
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
+import os
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    wait = WebDriverWait(driver, 10)
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+wait = WebDriverWait(driver, 10)
 
-    try:
+
+try:
         driver.get("https://playful-torrone-162c28.netlify.app")
         sleep(2)
-
+#----------------------------------------------LOGIN---------------------------------------------------------------------------
         driver.find_element(By.XPATH, '//span[text()="Login"]').click()
         driver.find_element(By.NAME, "email").send_keys("qualityassurance@bertoni.com.br")
         driver.find_element(By.NAME, "password").send_keys("versar123")
         driver.find_element(By.TAG_NAME, "form").submit()
-
+#-----------------------------------------------MODULO---------------------------------------------------------------------------
         criar_modulo_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.MuiButton-containedPrimary')))
         criar_modulo_btn.click()
 
@@ -41,24 +45,103 @@ def test_criar_modulo():
         ))
         botao_estrela.click()
 
-        for _ in range(2):
-            botao_salvar = wait.until(EC.element_to_be_clickable(
-                (By.XPATH, '//button[.//span[contains(text(),"Salvar")]]')
-            ))
-            botao_salvar.click()
-
-        botao_salvar_modulo = wait.until(EC.element_to_be_clickable(
-            (By.XPATH, '//button[.//span[contains(text(),"Salvar Módulo")]]')
+        botao_salvar = wait.until(EC.element_to_be_clickable(
+            (By.XPATH, '//button[.//span[contains(text(),"Salvar")]]')
         ))
-        botao_salvar_modulo.click()
+        botao_salvar.click()
+#---------------------------------------------------CRIAR TEMA PÓS MÓDULO-----------------------------------------------------------------
+        wait = WebDriverWait(driver, 10)
+        botao_criar_tema_modulo = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[.//span[contains(text(), 'Criar tema')]]")))
+        botao_criar_tema_modulo.click()
 
-        sleep(5)
+        titulo_input_titulotemamod = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Título do Tema"]')))
+        titulo_input_titulotemamod.send_keys("Módulo Automatizado QA - Tema")
+
+        campo_segmento = wait.until(EC.element_to_be_clickable((By.ID, "mui-component-select-segment_id")))
+        campo_segmento.click()
+        opcao_segmento = wait.until(EC.element_to_be_clickable((By.XPATH, "//li[contains(text(), 'Ensino Médio')]")))
+        opcao_segmento.click()
+
+        habilidades_dropdown = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#skill_ids .css-1hwfws3")))
+        actions = ActionChains(driver)
+        actions.double_click(habilidades_dropdown).perform()
+        menu = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".css-26l3qy-menu")))
+        opcao_habilidade = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[contains(@class, 'css-26l3qy-menu')]//div[text()='Autopercepção']")))
+        opcao_habilidade.click()
+        actions.send_keys(Keys.ESCAPE).perform()
+        time.sleep(2)  
+
+        driver.execute_script("window.scrollBy(0, 1000);") 
+        time.sleep(1)  
+
+        botao_capa = wait.until(EC.element_to_be_clickable((By.XPATH, '//div[contains(@class, "MuiGridListTileBar-actionIcon")]//button[contains(@class, "MuiIconButton-root")]')))
+        driver.execute_script("arguments[0].click();", botao_capa)
+
+        botao_salvar = wait.until(EC.element_to_be_clickable((By.XPATH,'//button[.//span[contains(text(), "Salvar")]]')))
+        botao_salvar.click()
+        time.sleep(5) 
+#-----------------------------------------------------CRIAR CONTEÚDO PÓS-TEMA-MODULO--------------------------------------------
+        wait = WebDriverWait(driver, 10)
+        botao_criar_conteudo_modulo = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[.//span[contains(text(), 'Criar conteúdo')]]")))
+        botao_criar_conteudo_modulo.click()
+    
+        titulo_input_titulocontmod = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Título do Conteúdo"]')))
+        titulo_input_titulocontmod.send_keys("Módulo Automatizado QA - Conteúdo")
+        
+        segmento_conteudo = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[.//span[contains(text(), 'Infantil')]]")))
+        segmento_conteudo.click()
+
+        driver.execute_script("window.scrollBy(0, 1000);") 
+        time.sleep(1)  
+
+        campo_foco_cont = wait.until(EC.element_to_be_clickable((By.ID, "mui-component-select-occupation_id")))
+        campo_foco_cont.click()
+        opcao_foco = wait.until(EC.element_to_be_clickable((By.XPATH, "//li[contains(text(), 'Estudante')]")))
+        opcao_foco.click()
+        
+        categoria_btn = wait.until(EC.element_to_be_clickable((By.ID, "mui-component-select-category_id")))
+        actions.double_click(categoria_btn).perform()
+        opcao_categoria = wait.until(EC.element_to_be_clickable((By.XPATH, "//li[contains(text(), 'Conhecer')]")))
+        opcao_categoria.click()
+        time.sleep(1)  
+
+        botao_avançar = wait.until(EC.element_to_be_clickable((By.XPATH,'//button[.//span[contains(text(), "Avançar")]]')))
+        botao_avançar.click()
+        time.sleep(5)
+#------------------------------------------------ PÁGINA COMPONENTES CONTEUDO-MODULO
+        wait = WebDriverWait(driver, 10)
+        def test_editor_nao_editavel(driver):
+            # Verifica se o editor está read-only
+            is_readonly = driver.execute_script("return tinymce.activeEditor.getMode() === 'readonly';")
+            if is_readonly:
+                pytest.fail("Erro: O campo de texto está em modo somente leitura (read-only)")
+
+            # Tenta inserir texto
+            driver.execute_script("tinymce.activeEditor.setContent('Teste automático');")
+            conteudo = driver.execute_script("return tinymce.activeEditor.getContent();")
+            assert "Teste automático" in conteudo, "Erro: Não conseguiu escrever no editor."
+
+
+            # Buscar arquivo documentos (FAZENDO AINDA)
+            documento_btn = wait.until(EC.element_to_be_clickable((By.XPATH,'//button[.//span[contains(text(), "Adicionar Arquivo")]]')))
+            documento_btn.click()
+            time.sleep(5)
+
+
+
+
+        #botao_salvar_modulo = wait.until(EC.element_to_be_clickable(
+            #(By.XPATH, '//button[.//span[contains(text(),"Salvar Módulo")]]')
+        #))
+        #botao_salvar_modulo.click()
+
+        #sleep(5)
 
         # Se chegou até aqui sem erro, o teste passou
-        assert True
+        #assert True
 
-    except Exception as e:
+except Exception as e:
         assert False, f"Erro no teste: {e}"
 
-    finally:
-        driver.quit()
+#finally:
+        #driver.quit()
